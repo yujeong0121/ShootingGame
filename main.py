d81d69ed4907bf5239786205cc63e5e24ccd9750
yujeong <skyggg3@gmail.com>
2021-11-27 19:32:11 +0900

    게임이름, 시간 추적, 음식 랜덤 생성, 운석위치 초기화, 음식이 밖으로 떨어질 경우 추가

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pygame
 import sys
+import random
 from time import sleep
 
-BLACK = (0,0,0)
 padWidth = 480 # 게임화면의 가로크기
 padHeight = 640 # 게임화면의 세로크기
-
+rockImage = ['icecream.png', 'fried-chicken.png ', 'pizza.png ', 'cake.png ', 'hamburger.png ',
+             'egg.png', 'broccoli.png', 'cabbage', 'carrot.png', 'vegetable.png'] # 음식 이미지 추가 - Yu
 
 # Sae 운석을 맞춘 개수 계산
 def writeScore(count):
@@ -27,15 +28,12 @@ def initGame():
     global gamePad, clock, background, fighter, missile, explosion
     pygame.init()
     gamePad = pygame.display.set_mode((padWidth, padHeight))
-    pygame.display.set_caption('PyShooting')
+    pygame.display.set_caption('PyShooting') # 게임 이름 추가 - Yu
+    clock = pygame.time.Clock() # 시간 추척 추가 - Yu
 
 
-    background = pygame.image.load('pig.png')
     pygame.init()   #Han
     gamePad = pygame #Han
-
-
-    pygame.display.set_caption('PyShooting')
 
     missile = pygame.image.load('spoon-and-fork.png') # Sae 미사일그림
 
@@ -51,7 +49,16 @@ def runGame():
     # Sae 무기좌표 리스트
     missileXY = []
 
+    # 음식 랜덤 생성 - Yu
+    rock = pygame.image.load(random.choice(rockImage))
+    rockSize = rock.get_rect().size # 운석 실제 크기
+    rockWidth = rockSize[0]
+    rockHeight = rockSize[1]
 
+    # 운석 초기 위치 설정 - Yu
+    rockX = random.randrange(0, padWidth - rockWidth)
+    rockY = 0
+    rockSpeed = 2
 
     onGame = False
     while not onGame:
@@ -61,6 +68,9 @@ def runGame():
                 sys.exit()
         drawObject(background, 0, 0) #배경화면 그리기 -Han
         pygame.display.update() #게임화면을 다시 그림 -Han
+        clock.tick(60) # 게임화면의 초당 프레임수를 60으로 설정 - Yu
+
+    pygame.quit() # pygame 종료 - Yu
 
             # 전투기 움직이기
             if event.type in [pygame.KEYDOWN]:
@@ -97,7 +107,23 @@ def runGame():
             for bx, by in missileXY:
                 drawObject(missile, bx, by)
 
-        # Sae 운석 맞춘 점수 표시
+        rockY += rockSpeed # 운석이 아래로 떨어질 때 y좌표 증가 - Yu
+
+        # 운석이 지구로 떨어진 경우(화면 밖으로) - Yu
+        if rockY > padHeight:
+            # 새로운 운석 생성(랜덤)
+            rock = pygame.image.load(random.choice(rockImage))
+            rockSize = rock.get_rect().size
+            rockWidth = rockSize[0]
+            rockHeight = rockSize[1]
+            rockX = random.randrange(0, padWidth - rockWidth)
+            rockY = 0
+
+        drawObject(rock, rockX, rockY) # 운석 그리기
+
+
+
+       # Sae 운석 맞춘 점수 표시
         writeScore(shotCount)
 
         # 운석이 지구로 떨어진 경우
