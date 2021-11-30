8ab198308d51f49fdf80311d07ceb5b8e5ec1eae
kimch963 <kimch963@gmail.com>
2021-11-30 09:06:10 +0900

    image change

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ padHeight = 640 # 게임화면의 세로크기
 
 rockImage = ['icecream.png', 'fried-chicken.png ', 'pizza.png ', 'cake.png ', 'hamburger.png ',
              'egg.png', 'broccoli.png', 'cabbage.png', 'carrot.png', 'vegetable.png',
-             'coke.png', 'candy.png', 'chocolate.png', 'hotdog.png', 'salad.png',
-             'fish.png', 'water.png', 'nuts.png', 'bananas.png', 'corn.png'] # 음식 이미지 추가 - Yu
+             'cola.png', 'candies.png', 'chocolate.png', 'hotdog.png', 'salad.png',
+             'fish.png', 'nuts.png', 'bananas.png', 'corn.png','glass-of-water'] # 음식 이미지 추가 - Yu
 
 
 
